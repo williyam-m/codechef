f8366c1115c0f8af8e7678769e35608fa1691ec7
williyam-m <williyam64@gmail.com>
2025-01-16 12:40:33 +0530

    create Entry Check.java

---
+++ java/Entry Check.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x >= 10) {
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+
+        }
+    }
+}
